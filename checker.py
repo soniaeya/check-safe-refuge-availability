c2cdd84051d97395c389aa3793421ec478053adf
Sonia Li <96924335+soniaeya@users.noreply.github.com>
2026-07-03 22:53:07 -0400

    Launch Chromium in headless mode

--- checker.py
+++ checker.py
@@ -382,7 +382,7 @@ url = ("https://aws-portal.owlpractice.ca/grichanditherapy/booking?therapist_id=
 
 async def main():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
+        browser = await p.chromium.launch(headless=True)
         page = await browser.new_page()
         await page.goto(url)
 
