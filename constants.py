14dbf180f926c0267d8fbfc9e756c7c389c40cdf
Sonia Li <96924335+soniaeya@users.noreply.github.com>
2026-07-16 13:57:37 -0400

    Add constants for URL and file paths

---
+++ constants.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+URL = ("https://aws-portal.owlpractice.ca/grichanditherapy/booking?therapist_id=1&location_id=1&rate_id=38&day=2026-07"
+       "-04&time=&video_session=1")
+STATE_FILE = Path("state.json")
+LOGS_FILE = Path("logs.json")
